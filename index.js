b354c90434558936bdab4b46c653e2ec63511af8
ChanvitChiiB <chanvit.ball34@gmail.com>
2024-11-08 02:12:25 +0700

    first commit

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.get('/get_messages', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server has started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port", port);
 });
